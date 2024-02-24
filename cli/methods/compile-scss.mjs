7b5c45e56833edd1500ed805a2bd19e4364593fc
Stijn <stijn.slats@gmail.com>
2024-02-24 19:37:37 +0100

    Update compile-scss.mjs

--- cli/methods/compile-scss.mjs
+++ cli/methods/compile-scss.mjs
@@ -1,17 +1,56 @@
+// File system packages
+import fse from "fs-extra";
 import { globSync } from "glob";
+
+// Compilers
+import * as sass from "sass";
+
+// PostCSS
+import postcss from "postcss";
+import autoprefixer from "autoprefixer";
+import postcssPresetEnv from "postcss-preset-env";
+import tailwind from "tailwindcss";
 
 export default class CompileScss {
   constructor() {
     this.files = globSync("src/assets/scss/**/*.scss", {
+      ignore: [
+        "src/assets/scss/{partials,tailwind}/**",
+        "src/assets/scss/**/_*.scss",
+      ],
       posix: true,
     });
   }
 
   async run() {
-    await this.compile();
+    await this.compileFile();
   }
 
-  async compile() {
-    await new Promise((resolve) => setTimeout(resolve, 1500));
+  async compileFile() {
+    for (const file of this.files) {
+      try {
+        const { css } = await sass.compileAsync(file);
+        await postcss([
+          tailwind(),
+          autoprefixer(),
+          postcssPresetEnv({ features: { "cascade-layers": false } }),
+        ])
+          .process(css, { from: file })
+          .then(({ css }) => {
+            // Create file name based on the directory structure. E.g. sections-files.scss
+            const fileName = file
+              .replace("src/assets/scss/", "")
+              .replace("theme/", "")
+              .replace(".scss", ".css");
+            const outputFileName = fileName.replace(/\//g, "-");
+            const outputFile = `theme/assets/${outputFileName}`;
+
+            // Write the file
+            fse.outputFile(outputFile, css);
+          });
+      } catch (error) {
+        console.error(error); // Handle errors appropriately
+      }
+    }
   }
 }
