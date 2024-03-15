afd64f31f91a1208360400e577ee0404127cfb89
Stijn <stijn@code.nl>
2024-03-15 13:25:10 +0100

    Create envPrompt.mjs

---
+++ cli/scripts/envPrompt.mjs
@@ -0,0 +1,31 @@
+
+import fse from "fs-extra";
+import prompts from "prompts";
+import TOML from "@iarna/toml";
+
+const tomlFile = fse.readFileSync("./shopify.theme.toml", "utf-8");
+const { environments } = TOML.parse(tomlFile);
+
+async function getEnvironment() {
+
+  const choices = Object.entries(environments).map(([env, options]) => {
+    return {
+      title: `${env} - ${options.store}`,
+      value: {
+        env,
+        store: options.store,
+      },
+    };
+  });
+
+  return await prompts({
+    type: 'select',
+    name: 'value',
+    message: 'Select environment:',
+    choices
+  });
+}
+
+
+
+export { getEnvironment };
