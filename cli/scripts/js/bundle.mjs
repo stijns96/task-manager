0e89d32b34414e6cfda2eeb9f44cc13e11f32c6f
Stijn <stijn@code.nl>
2024-03-17 21:39:38 +0100

    Add eslint

--- cli/scripts/js/bundle.mjs
+++ cli/scripts/js/bundle.mjs
@@ -1,15 +1,14 @@
 import config from "../../config.mjs";
-
 import { rollup } from "rollup";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-
-import chalk from "chalk";
+import { ESLint } from 'eslint';
 import { globSync } from "glob";
 
 export default class BundleJs {
   constructor({ input } = {}) {
     this.input = input ? [input] : globSync(config.assets.js.glob.input, config.assets.js.glob.options);
 
+    this.eslintInstance = new ESLint({ useEslintrc: true, fix: true });
     this.bundle;
     this.errors = [];
 
@@ -20,11 +19,9 @@ export default class BundleJs {
         plugins: [nodeResolve()],
         treeshake: false,
       },
-      outputOptionsList: [
-        {
-          dir: config.theme.assetsDir,
-        },
-      ],
+      outputOptions: {
+        dir: config.theme.assetsDir,
+      },
     };
   }
 
@@ -34,34 +31,53 @@ export default class BundleJs {
 
   async build() {
     try {
+      await this.lintFiles();
+
       this.bundle = await rollup(this.rollup.inputOptions);
-      await this.generateOutputs(this.bundle);
-    } catch (error) {
-      const { frame, loc, message, stack } = error;
+      await this.bundle.write(this.rollup.outputOptions);
 
-      const errorMessages = `${chalk.red(`Bundle error:`)} ${message}`;
-      const position = `${loc.line}:${loc.column}`;
-      const file = loc.file.replace(process.cwd(), "");
-
-      this.errors.push(
-        `${errorMessages} in ${chalk.blue.underline(
-          `${file}:${position}`
-        )}\n\n${frame}\n\n${chalk.dim(stack)}`
-      );
     } finally {
       if (this.bundle) {
         await this.bundle.close();
       }
 
       if (this.errors.length > 0) {
+        // eslint-disable-next-line no-unsafe-finally
         throw this.errors;
       }
     }
   }
 
-  async generateOutputs(bundle) {
-    for (const outputOptions of this.rollup.outputOptionsList) {
-      await bundle.write(outputOptions);
+  // Lint the specified files and return the results
+  async lintAndFix(eslint, filePaths) {
+    const results = await eslint.lintFiles(filePaths);
+
+    // Apply automatic fixes and output fixed code
+    await ESLint.outputFixes(results);
+
+    return results;
+  }
+
+  // Log results to console if there are any problems
+  async outputLintingResults(results) {
+    // Identify the number of problems found
+    const problems = results.reduce((acc, result) => acc + result.errorCount + result.warningCount, 0);
+
+    if (problems > 0) {
+      const eslintFormatter = await this.eslintInstance.loadFormatter("stylish");
+      const output = await eslintFormatter.format(results);
+
+      if (!output) return;
+
+      this.errors.push(output);
     }
+
+    return results;
+  }
+
+  // Put previous functions all together
+  async lintFiles() {
+    const results = await this.lintAndFix(this.eslintInstance, this.input);
+    return this.outputLintingResults(results);
   }
 }
