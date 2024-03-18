3b9f56d38122b23bf47ab44dae6d60af8edbcceb
Stijn <stijn@code.nl>
2024-03-18 16:54:52 +0100

    Change old index to use commander

--- cli/index.mjs
+++ cli/index.mjs
@@ -1,107 +1,56 @@
-import Dev from "./commands/dev.mjs";
-import Build from "./commands/build.mjs";
-import Clean from "./commands/clean.mjs";
-import Push from "./commands/push.mjs";
-import PushAll from "./commands/pushAll.mjs";
-import Pull from "./commands/pull.mjs";
-import OpenAdmins from "./commands/openAdmins.mjs";
+import { program } from 'commander';
+import terminalLink from 'terminal-link';
 
-// Terminal packages
-import yargs from "yargs/yargs";
-import { hideBin } from "yargs/helpers";
+program
+  .name('themify')
+  .description('CLI to develop and build themes for Shopify.')
+  .version('0.0.0');
 
-class CLI {
-  constructor() {
-    // Get arguments from the command line
-    this.argv = yargs(hideBin(process.argv)).argv;
+program
+  .command('dev')
+  .description(`Uploads the current theme as the specified theme, or a ${terminalLink('development theme', 'https://shopify.dev/docs/themes/tools/cli#development-themes')}, to a store so you can preview it.`)
+  .option("-e, --environment <env_name>", `The ${terminalLink('environment', 'https://shopify.dev/docs/themes/tools/cli/environments')} from your ${terminalLink('shopify.theme.toml', 'shopify.theme.toml')} that you want to use.`)
+  .action(async () => {
+    const Dev = await import(`./commands/dev.mjs`);
+    const dev = new Dev.default();
+    dev.run();
+  });
 
-    // Set arguments to an object
-    this.args = {
-      dev: this.argv.dev || false,
-      build: this.argv.build || false,
-      clean: this.argv.clean || false,
-      push: this.argv.push || false,
-      pull: this.argv.pull || false,
-      openAdmins: this.argv.openAdmins || false,
-    };
+program
+  .command('build')
+  .description('Build the src folder to the theme folder to make it ready for uploading it to a Shopify.')
+  .action(async () => {
+    const Build = await import(`./commands/build.mjs`);
+    const build = new Build.default();
+    build.run();
+  });
 
-    this.run();
-  }
+program
+  .command('clean')
+  .description('Clean the theme folder to make it ready for a new build.')
+  .action(async () => {
+    const Clean = await import(`./commands/clean.mjs`);
+    const clean = new Clean.default();
+    clean.run();
+  });
 
-  /**
-   * Run the CLI
-   */
-  run() {
-    this.args.dev && this.dev();
-    this.args.build && this.build();
-    this.args.clean && this.clean();
-    this.args.push && this.push();
-    this.args.pull && this.pull();
-    this.args.openAdmins && this.openAdmins();
-  }
+program
+  .command('push')
+  .description('Uploads your local theme files to Shopify, overwriting the remote version if specified.')
+  .option("-e, --environment <env_name>", `The ${terminalLink('environment', 'https://shopify.dev/docs/themes/tools/cli/environments')} from your ${terminalLink('shopify.theme.toml', 'shopify.theme.toml')} that you want to push to.`)
+  .action(async () => {
+    const Push = await import(`./commands/push.mjs`);
+    const push = new Push.default();
+    push.run();
+  });
 
-  /**
-   * Run development
-   */
-  async dev() {
-    const dev = new Dev();
+program
+  .command('pull')
+  .description('Retrieves theme files from Shopify in the theme folder.')
+  .action(async () => {
+    const Pull = await import(`./commands/pull.mjs`);
+    const pull = new Pull.default();
+    pull.run();
+  });
 
-    await dev.run();
-  }
-
-  /**
-   * Build assets
-   */
-  async build() {
-    const build = new Build();
-
-    // Clean assets before building
-    await this.clean();
-
-    await build.run();
-  }
-
-  /**
-   * Clean assets
-   */
-  async clean() {
-    const clean = new Clean();
-
-    await clean.run();
-  }
-
-  /**
-   * Push to Shopify
-   */
-  async push() {
-    await this.build();
-
-    if (this.argv.push === true) {
-      const push = new Push();
-      await push.run();
-    } else {
-      const pushAll = new PushAll();
-      await pushAll.run();
-    }
-  }
-
-  /**
-   * Pull from Shopify
-   */
-  async pull() {
-    const pull = new Pull();
-
-    await pull.run();
-  }
-
-  /**
-   * Open Shopify admins
-   */
-  async openAdmins() {
-    const openAdmins = new OpenAdmins();
-
-    await openAdmins.run();
-  }
-}
-
-new CLI();
+program.parse(process.argv);
