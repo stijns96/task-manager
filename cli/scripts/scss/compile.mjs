db322075dcb07bb33ec38fc2f6ea7d485ab09fa1
Stijn <stijn@code.nl>
2024-03-26 21:55:23 +0100

    Refactor scss/compile

--- cli/scripts/scss/compile.mjs
+++ cli/scripts/scss/compile.mjs
@@ -1,22 +1,18 @@
-import config from "../../config.mjs";
-import { pathToFileURL } from 'url';
-
 // File system packages
 import fse from "fs-extra";
-
-// Compilers
-import * as sass from "sass";
-
-// PostCSS
-import postcss from "postcss";
-import autoprefixer from "autoprefixer";
-import postcssPresetEnv from "postcss-preset-env";
-import tailwind from "tailwindcss";
 import { globSync } from "glob";
+
+import config from "../../config.mjs";
+import compileScss from "../../utils/compileScss.mjs";
 
 export default class CompileScss {
   constructor({ input } = {}) {
-    this.input = input ? [input] : globSync(config.assets.scss.glob.input, config.assets.scss.glob.options);
+    this.input = input
+      ? [input]
+      : globSync(
+          config.assets.scss.glob.input,
+          config.assets.scss.glob.options,
+        );
 
     this.errors = [];
   }
@@ -28,62 +24,36 @@ export default class CompileScss {
   async compileFile() {
     for (const file of this.input) {
       try {
-        // Compile the file
-        const { css } = await sass.compileAsync(file, {
-          importers: [{
-            // An importer that redirects relative URLs starting with "~" to
-            // `node_modules`.
-            findFileUrl(url) {
-              if (!url.startsWith('~')) return null;
-              const baseUrl = pathToFileURL('node_modules/'); // Construct base URL
-              const resolvedUrl = new URL(url.substring(1), baseUrl); // Use base URL to resolve
-              return resolvedUrl;
-            }
-          }]
-        });
+        const css = await compileScss({ file });
 
+        // Create file name based on the directory structure. E.g. sections-files.scss
+        const fileName = file
+          .replace(`${config.src.assetsDir}/scss/`, "")
+          .split("/")
+          .join("-")
+          .replace(".scss", ".css");
 
-        // Process the CSS
-        await postcss([
-          tailwind(),
-          autoprefixer(),
-          postcssPresetEnv({ features: { "cascade-layers": false } }),
-        ])
-          .process(css, { from: file })
-          .then(({ css }) => {
-            // Create file name based on the directory structure. E.g. sections-files.scss
-            const fileName = file
-              .replace(`${config.src.assetsDir}/scss/`, "")
-              .replace(".scss", ".css");
+        // Array of folder names
+        const folderNames = file.split("/");
 
-            // Replace slashes with dashes
-            const outputFileName = fileName.replace(/\//g, "-");
+        // Make sure that the actual file name can not be the layer name
+        folderNames.pop();
 
-            // Array of folder names
-            const folderNames = file.split("/");
+        // Rename snippets to components.
+        // If the layer name is empty, use an empty string
+        const layerName =
+          folderNames[4] === "snippets" ? "components" : folderNames[4] || "";
 
-            // Make sure that the actual file name can not be the layer name
-            folderNames.pop();
+        // Create the layer
+        const layer = `@layer ${layerName} {\n${css}\n}`;
 
-            // Rename snippets to components.
-            // If the layer name is empty, use an empty string
-            const layerName =
-              folderNames[4] === "snippets"
-                ? "components"
-                : folderNames[4] || "";
+        const data = layerName ? layer : css;
 
-            // Create the layer
-            const layer = `@layer ${layerName} {\n${css}\n}`;
-
-            const data = layerName ? layer : css;
-
-            // Write the file
-            fse.outputFile(`${config.theme.assetsDir}/${outputFileName}`, data);
-          });
+        // Write the file
+        fse.outputFile(`${config.theme.assetsDir}/${fileName}`, data);
       } catch (error) {
         this.errors.push(error);
       }
-
     }
 
     if (this.errors.length > 0) {
